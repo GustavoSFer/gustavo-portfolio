2692172a582bd251545996d02c118fbf1d989109
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-12-17 18:54:50 -0300

    gh-pages

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gustavo-portfolio">
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
