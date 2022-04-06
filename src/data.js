3a96c87831ec7e11f628f2a6f0a862a5146c9069
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-04-06 19:53:05 -0300

    projeto digimatec

--- src/data.js
+++ src/data.js
@@ -1,20 +1,28 @@
 import recipes from './imagens/app-recipes.png';
 import soseries from './imagens/soseries.png';
+import blogDigimatec from './imagens/Digimatec.png'
 
 const data = [
   {
     id: 1,
     nameProject: 'App Recipes',
-    description:'desenvolver um app de receitas, sendo com buscas, filtragem, favoritar e acompanhar o processo de preparação de receitas e drinks!',
+    description:'Desenvolver um app de receitas, sendo com buscas, filtragem, favoritar e acompanhar o processo de preparação de receitas e drinks!',
     URL: 'https://GustavoSFer.github.io/recipe-app',
     img: recipes
   },
   {
     id: 2,
     nameProject: 'So Séries',
-    description:'Colocar em praticar CSS e HTML',
+    description:'Clone Netflix, Atividade da Dio. Fazer uma pagina igual ao do Netflix colocando em pratico o que aprendi no curso.',
     URL: 'https://gustavosfer.github.io/SoSeries/index.html',
     img: soseries
+  },
+  {
+    id: 3,
+    nameProject: 'Blog Digimatec',
+    description:'Desenvolver um blog para ser utilizado pelos cliente da empresa. Conseguindo ver tutoriais e videos.',
+    URL: 'http://www.digimatec.com.br/blog/',
+    img: blogDigimatec
   },
 ];
 
